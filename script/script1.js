437a16bb7ad19e5179778f0c5839d98e4b568929
astro-v <valentinmichel07200@gmail.com>
2021-09-11 16:41:40 +0200

    commit

--- script/script1.js
+++ script/script1.js
@@ -1,42 +1,41 @@
 let titreArticle = document.getElementsByClassName('titreArticle1');
 let texteArticle = document.getElementsByClassName('texte1');
 
-let dmc_setInterval_1;
+let dmc_setInterval_11;
+let dmc_setInterval_12;
 let bool = true;
 window.addEventListener('scroll', function() {
 	if (this.scrollY > 800 && bool) {
 		bool = false;
 		titreArticle[0].style.marginTop = '800px';
 		titreArticle[0].style.opacity = '0';
-		dmc_setInterval_1 = setInterval(function() {
+		dmc_setInterval_11 = setInterval(function() {
 			if (parseInt(titreArticle[0].style.marginTop,10)>50 && bool == false) {
 				titreArticle[0].style.marginTop = (parseInt(titreArticle[0].style.marginTop,10)-4).toString()+'px';
 			} else {
 				titreArticle[0].style.marginTop = '50px';
-				clearInterval(dmc_setInterval_1);
 			}
 			if (titreArticle[0].style.opacity<1 && bool == false ) {
 				titreArticle[0].style.opacity = (parseFloat(titreArticle[0].style.opacity,10)+4/(800-50)).toString();
 			} else {
 				titreArticle[0].style.opacity = '1';
+				clearInterval(dmc_setInterval_11);
 			}
 		},1);	
 	}
 	if (this.scrollY <= 800 && bool == false) {
 		bool = true;
-		titreArticle[0].style.marginTop = '50px';
-		titreArticle[0].style.opacity = '1';
-		dmc_setInterval_1 = setInterval(function() {
+		dmc_setInterval_12 = setInterval(function() {
 			if (parseInt(titreArticle[0].style.marginTop,10)<800 && bool) {
 				titreArticle[0].style.marginTop = (parseInt(titreArticle[0].style.marginTop,10)+4).toString()+'px';
 			} else {
 				titreArticle[0].style.marginTop = '800px';
-				clearInterval(dmc_setInterval_1);
 			}
 			if (titreArticle[0].style.opacity>0 && bool) {
 				titreArticle[0].style.opacity = (parseFloat(titreArticle[0].style.opacity,10)-4/(800-50)).toString();
 			} else {
 				titreArticle[0].style.opacity = '0';
+				clearInterval(dmc_setInterval_12);
 			}
 		},1);	
 	}
