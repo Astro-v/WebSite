37c43876357246ce6d0300c6b298e424f095a105
astro-v <valentinmichel07200@gmail.com>
2021-09-11 12:00:45 +0200

    test

--- script/script1.js
+++ script/script1.js
@@ -2,8 +2,8 @@ let titreArticle = document.getElementsByClassName('titreArticle1');
 let texteArticle = document.getElementsByClassName('texte1');
 
 window.addEventListener('scroll', function() {
-	if (this.scrollY > 200 && this.scrollY < 600) {
-		titreArticle[0].style.marginTop = (800-this.scrollY).toString()+'px';
+	if (this.scrollY > 300 && this.scrollY < 800) {
+		titreArticle[0].style.marginTop = (825-this.scrollY).toString()+'px';
 	}
 	if (this.scrollY > 450 && this.scrollY < 600) {
 		titreArticle[0].style.opacity = ((this.scrollY-450)/150).toString();
---
+++ script/script3.js
@@ -0,0 +1,5 @@
+let decouvrirButton = document.getElementsByClassName('decouvrirButton');
+
+decouvrirButton[0].addEventListener('click', function() {
+	window.scroll(0,1000)
+});
