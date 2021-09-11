3aab2e50e3fce1030ab60518ed5d69e35344e809
astro-v <valentinmichel07200@gmail.com>
2021-09-11 11:38:05 +0200

    Avancement

--- script/script1.js
+++ script/script1.js
@@ -1,22 +1,18 @@
 let titreArticle = document.getElementsByClassName('titreArticle1');
-
-window.addEventListener('scroll', function() {
-	if (this.scrollY > 200 && this.scrollY < 600) {
-		titreArticle[0].style.marginTop = 800-this.scrollY+'px';
-	}
-	if (this.scrollY > 450 && this.scrollY < 600) {
-		titreArticle[0].style.opacity = (this.scrollY-450)/150;
-	}
-});
-
 let texteArticle = document.getElementsByClassName('texte1');
 
 window.addEventListener('scroll', function() {
 	if (this.scrollY > 200 && this.scrollY < 600) {
-		titreArticle[0].style.marginTop = 800-this.scrollY+'px';
+		titreArticle[0].style.marginTop = (800-this.scrollY).toString()+'px';
 	}
 	if (this.scrollY > 450 && this.scrollY < 600) {
-		titreArticle[0].style.opacity = (this.scrollY-450)/150;
+		titreArticle[0].style.opacity = ((this.scrollY-450)/150).toString();
+	}
+	if (this.scrollY > 500 && this.scrollY < 900) {
+		texteArticle[0].style.marginTop = (1100-this.scrollY).toString()+'px';
+	}
+	if (this.scrollY > 750 && this.scrollY < 900) {
+		texteArticle[0].style.opacity = (this.scrollY-750)/150;
 	}
 });
 
